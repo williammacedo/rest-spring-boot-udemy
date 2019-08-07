dfcf2efe137b7622e60fae2aac53844e2b61d3cf
William França Macedo <william@localhost.localdomain>
2019-08-06 23:07:59 -0300

    Initializing Spring Boot Project

---
+++ src/main/java/br/com/example/restspringbootudemy/RestSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.restspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSpringBootUdemyApplication.class, args);
+    }
+
+}
